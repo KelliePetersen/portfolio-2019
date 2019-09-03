4e5972ac79f8a6a6463f79c3f30d62bf0a53e148
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-09-03 14:20:19 -0400

    add validity check for form

--- app/src/js/modules/ContactForm.js
+++ app/src/js/modules/ContactForm.js
@@ -7,16 +7,14 @@ class ContactForm {
   events() {
     this.form[0].addEventListener("focusin", (event) => {
       event.target.previousElementSibling.classList.add("contact__label--is-focus");
-      event.target.previousElementSibling.classList.add("contact__label--is-focus");
     });
     this.form[0].addEventListener("focusout", (event) => {
       if (event.target.value.length === 0) {
         event.target.previousElementSibling.classList.remove("contact__label--is-focus");
-        event.target.previousElementSibling.classList.remove("contact__label--is-focus");
       }
     });
     this.form[0].addEventListener("input", (event) => {
-      if (event.target.value.length === 0) {
+      if (!event.target.validity.valid) {
         event.target.classList.add("contact--required");
         event.target.previousElementSibling.classList.add("contact__label--required");
       } else {
