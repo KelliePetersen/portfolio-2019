e5cfbf009c2da4109934bbfcbaeb57fb2066b862
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-09-03 15:52:42 -0400

    add animated buttons

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -3,4 +3,5 @@ import MobileMenu from './modules/MobileMenu';
 import ContactForm from './modules/ContactForm';
 
 const backToTop = new BackToTop();
+const mobileMenu = new MobileMenu();
 const contactForm = new ContactForm();
